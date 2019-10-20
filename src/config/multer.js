6c88efea7c8cc7fbd06f75fd27b8778af326f5ac
Librity <luis.geniole@gmail.com>
2019-10-20 16:12:05 -0300

    create&dev file&provider controllers w/db:migrations

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,5 +3,14 @@ import crypto from 'crypto';
 import { extname, resolve } from 'path';
 
 export default {
-  storage: 
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
 };

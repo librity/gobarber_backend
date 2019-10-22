a78d0ee0773e85650bbb2c1bedd80d51966086c1
Luis Geniole <luis.geniole@gmail.com>
2019-10-21 22:46:18 -0300

    add appointment-delete action

--- src/routes.js
+++ src/routes.js
@@ -29,8 +29,10 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.get('/schedules', ScheduleController.index);
 

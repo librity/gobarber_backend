74ee8df91a007e955ab7530aacdda25697508d41
Luis Geniole <luis.geniole@gmail.com>
2019-10-21 23:38:42 -0300

    add&configure handlebars' templates

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,15 +1,40 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+
 import mailConfig from '../config/mail';
 
 class Mail {
   constructor() {
     const { host, port, secure, auth } = mailConfig;
+
     this.transporter = nodemailer.createTransport({
       host,
       port,
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewsPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsPath: resolve(viewsPath, 'layouts'),
+          partialsPath: resolve(viewsPath, 'partials'),
+          defaultLayout: 'deafault',
+          extname: '.hbs',
+        }),
+        viewsPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendMail(message) {

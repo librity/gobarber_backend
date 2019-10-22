67290ad676f3c7064af0d4b758218ebdb9d64279
Luis Geniole <luis.geniole@gmail.com>
2019-10-21 23:15:01 -0300

    add &configure Mailtrap mailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '10b10bd82699e2',
+    pass: 'c69ad7bfab860a',
+  },
+  default: {
+    from: 'Equipe GOBarber <noreply@gobarber.com>',
+  },
+};

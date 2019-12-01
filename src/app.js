086eb4849c1c6e93a567fe40b31dfbc3c006dbc1
Luis Geniole <luis.geniole@gmail.com>
2019-11-30 23:11:46 -0300

    add helmet

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import helmet from 'helmet';
 import Youch from 'youch';
 
 import * as Sentry from '@sentry/node';
@@ -28,6 +29,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
+    this.server.use(helmet());
     this.server.use(express.json());
     this.server.use(
       '/files',
